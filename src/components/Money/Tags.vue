9634362a7659e409c7265978e744aa6166198ed8
waiting <539959237@qq.com>
2020-02-19 16:38:59 +0800

    完成type组件ts部分

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="tags">
+    <div class="new-add">
+      <button>新增标签</button>
+    </div>
+    <ul class="current">
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Tags'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .tags{
+    font-size: 14px;
+    padding: 16px;
+    border: 1px solid green;
+    display: flex;
+    flex-grow: 1;
+    flex-direction: column-reverse;
+    > .current{
+      display: flex;
+      flex-wrap: wrap;
+      > li{
+        background: #d9d9d9;
+        $labels-height:24px;
+        height:$labels-height;
+        border-radius:$labels-height;
+        padding: 1px 16px;
+        margin-right: 12px;
+        margin-top: 4px;
+      }
+    }
+    > .new-add{
+      padding-top: 16px;
+      button{
+        background: transparent;
+        border:none;
+        color:#999;
+        border-bottom: 1px solid;
+        padding: 0 4px;
+      }
+    }
+  }
+</style>

a24ee0fb9b2f4bed089cb14fa017ae8dcbe74d85
waiting <539959237@qq.com>
2020-02-27 14:35:18 +0800

    展示tag的name

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,9 @@
   <Layout class-prefix="layout">
     <NumberPad @update:value="onUpdateNumberPad" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
-    <FormItem field-name="备注" placeholder="请在这里输入备注" @update:value="onUpdateNotes"/>
+    <FormItem field-name="备注"
+              placeholder="请在这里输入备注"
+              @update:value="onUpdateNotes"/>
     <Tags :dataSource.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>

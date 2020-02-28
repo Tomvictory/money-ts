c27ec48d80f81a610aff4cd83cf123aadec54985
waiting <539959237@qq.com>
2020-02-28 19:32:40 +0800

    封装tagList的增删改查

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -21,4 +21,7 @@ type TagListModel = {
 interface Window {
   tagList: Tag[];
   createTag: (name: string) => void;
+  removeTag: (id: string) => boolean;
+  updateTag: (id: string,name: string) => 'success' | 'not found' | 'dupliceted';
+  findTag: (id: string) => Tag | undefined;
 }

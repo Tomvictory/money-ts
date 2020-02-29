a747fbda9cc0223ea82b4383dac7ee57cbeabfbd
waiting <539959237@qq.com>
2020-02-29 17:22:31 +0800

    模块化的STORE 删除了models

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,27 +1,9 @@
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+import tagStore from '@/store/tagStore';
+import recordStore from '@/store/recordStore';
 
 const store = {
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => recordListModel.create(record),
-  tagList: tagListModel.fetch(),
-  createTag: (name: string) => {
-    const message = tagListModel.create(name);
-    if (message === 'dupliceted') {
-      window.alert('标签名重复');
-    } else if (message === 'success') {
-      window.alert('添加成功');
-    }
-  },
-  findTag: (id: string) => {
-    return store.tagList.filter(t => t.id === id)[0];
-  },
-  removeTag: (id: string) => {
-    return tagListModel.remove(id);
-  },
-  updateTag: (id: string, name: string) => {
-    return tagListModel.update(id, name);
-  }
+  ...tagStore,
+  ...recordStore
 };
 
 export default store;
